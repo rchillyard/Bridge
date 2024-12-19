f503f2b1512dbb0b7491d4521646ff47cfde8475
Robin Hillyard <r.hillyard@northeastern.edu>
2024-12-19 09:42:18 -0500

    More cosmetics (unit tests)
    Split PlayResultSpec and EventSpec into CheckerSpec

--- src/test/scala/com/phasmidsoftware/bridge/director/PlayResultSpec.scala
+++ src/test/scala/com/phasmidsoftware/bridge/director/PlayResultSpec.scala
@@ -4,145 +4,93 @@
 
 package com.phasmidsoftware.bridge.director
 
-import com.phasmidsoftware.bridge.director.Checker._
 import com.phasmidsoftware.bridge.director.Vulnerability._
-import com.phasmidsoftware.misc.JPredicate
+import com.phasmidsoftware.number.core.Rational
+import com.phasmidsoftware.output.MockWriter
+import com.phasmidsoftware.util.Output
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
 class PlayResultSpec extends AnyFlatSpec with should.Matchers {
 
-  behavior of "Game"
-
-  it should "work for game NS" in {
-    val predicate = Checker.gameP(true)
-    predicate.justification(ScoreVul(600, Vulnerability.N)) shouldBe Some("NS game 3 NT")
-    predicate(ScoreVul(600, Vulnerability.N)) shouldBe true
-    predicate(ScoreVul(620, Vulnerability.N)) shouldBe true
-    predicate(ScoreVul(450, Vulnerability.E)) shouldBe true
-    predicate(ScoreVul(480, Vulnerability.E)) shouldBe true
-    predicate(ScoreVul(1430, Vulnerability.N)) shouldBe true
-    predicate(ScoreVul(2210, Vulnerability.N)) shouldBe true
-    predicate(ScoreVul(-50, Vulnerability.N)) shouldBe false
-  }
-  it should "work for game EW" in {
-    val predicate = Checker.gameP(false)
-    predicate.justification(ScoreVul(-600, Vulnerability.E)) shouldBe Some("EW game 3 NT")
-    predicate(ScoreVul(-600, Vulnerability.E)) shouldBe true
-    predicate(ScoreVul(-620, Vulnerability.E)) shouldBe true
-    predicate(ScoreVul(-650, Vulnerability.E)) shouldBe true
-    predicate(ScoreVul(-680, Vulnerability.E)) shouldBe true
-    predicate(ScoreVul(-980, Vulnerability.N)) shouldBe true
-    predicate(ScoreVul(-1510, Vulnerability.N)) shouldBe true
-    predicate(ScoreVul(50, Vulnerability.E)) shouldBe false
-  }
-
-  behavior of "Penalty"
-
-  it should "work for NS penalty when not vul" in {
-    Penalty.justification(ScoreVul(50, Vulnerability.O)) shouldBe Some("EW  down 1")
-    Penalty(ScoreVul(50, Vulnerability.O)) shouldBe true
-    Penalty(ScoreVul(-50, Vulnerability.O)) shouldBe true
-  }
-
-  behavior of "penaltyP"
-
-  it should "work for NS penalty when not vul" in {
-    penaltyP(true)(ScoreVul(-50, Vulnerability.O)) shouldBe true
-    penaltyP(true)(ScoreVul(-100, Vulnerability.O)) shouldBe true
-    penaltyP(true)(ScoreVul(-150, Vulnerability.O)) shouldBe true
-    penaltyP(true)(ScoreVul(-200, Vulnerability.O)) shouldBe true
-    penaltyP(true)(ScoreVul(-250, Vulnerability.O)) shouldBe true
-    penaltyP(true)(ScoreVul(-300, Vulnerability.O)) shouldBe true
-    penaltyP(true)(ScoreVul(-350, Vulnerability.O)) shouldBe true
-  }
-
-  it should "work for NS penalty when vul" in {
-    penaltyP(true)(ScoreVul(-50, Vulnerability.N)) shouldBe false
-    penaltyP(true)(ScoreVul(-100, Vulnerability.N)) shouldBe true
-    penaltyP(true)(ScoreVul(-150, Vulnerability.N)) shouldBe false
-    penaltyP(true)(ScoreVul(-200, Vulnerability.N)) shouldBe true
-  }
-
-  it should "work for NS doubled penalty when not vul" in {
-    penaltyP(true)(ScoreVul(-100, Vulnerability.O)) shouldBe true
-    penaltyP(true)(ScoreVul(-300, Vulnerability.O)) shouldBe true
-    penaltyP(true)(ScoreVul(-500, Vulnerability.O)) shouldBe true
-  }
-
-  it should "work for NS doubled penalty when vul" in {
-    penaltyP(true)(ScoreVul(-200, Vulnerability.N)) shouldBe true
-    penaltyP(true)(ScoreVul(-500, Vulnerability.N)) shouldBe true
-    penaltyP(true)(ScoreVul(-800, Vulnerability.N)) shouldBe true
-    penaltyP(true)(ScoreVul(-1100, Vulnerability.N)) shouldBe true
-  }
-
-  behavior of "Partial"
-
-  it should "work for NS notrump partials" in {
-    val result = Partial.justification(ScoreVul(90, Vulnerability.N))
-    result.isDefined shouldBe true
-    result.get shouldBe "NS partial 1 NT"
-    Partial(ScoreVul(120, Vulnerability.N)) shouldBe true
-    Partial(ScoreVul(150, Vulnerability.N)) shouldBe true
-    Partial(ScoreVul(180, Vulnerability.N)) shouldBe true
-    Partial(ScoreVul(210, Vulnerability.N)) shouldBe true
-    Partial(ScoreVul(240, Vulnerability.N)) shouldBe true
-    Partial(ScoreVul(270, Vulnerability.N)) shouldBe true
-  }
-
-  it should "work for EW notrump partials" in {
-    Partial(ScoreVul(-90, Vulnerability.N)) shouldBe true
-    Partial(ScoreVul(-120, Vulnerability.N)) shouldBe true
-    Partial(ScoreVul(-150, Vulnerability.N)) shouldBe true
-    Partial(ScoreVul(-180, Vulnerability.N)) shouldBe true
-    Partial(ScoreVul(-210, Vulnerability.N)) shouldBe true
-    Partial(ScoreVul(-240, Vulnerability.N)) shouldBe true
-    Partial(ScoreVul(-270, Vulnerability.N)) shouldBe true
-  }
-
-  it should "work for NS partials" in {
-    Partial(ScoreVul(70, Vulnerability.N)) shouldBe true
-    Partial(ScoreVul(80, Vulnerability.N)) shouldBe true
-    Partial(ScoreVul(110, Vulnerability.N)) shouldBe true
-    Partial(ScoreVul(140, Vulnerability.N)) shouldBe true
-    Partial(ScoreVul(170, Vulnerability.N)) shouldBe true
-    Partial(ScoreVul(200, Vulnerability.N)) shouldBe true
-    Partial(ScoreVul(230, Vulnerability.N)) shouldBe true
-    Partial(ScoreVul(260, Vulnerability.N)) shouldBe true
-    Partial(ScoreVul(-50, Vulnerability.N)) shouldBe false
-  }
-
-  it should "work for EW partials" in {
-    Partial(ScoreVul(-70, Vulnerability.N)) shouldBe true
-    Partial(ScoreVul(-80, Vulnerability.N)) shouldBe true
-    Partial(ScoreVul(-110, Vulnerability.N)) shouldBe true
-    Partial(ScoreVul(-140, Vulnerability.N)) shouldBe true
-    Partial(ScoreVul(-170, Vulnerability.N)) shouldBe true
-    Partial(ScoreVul(-200, Vulnerability.N)) shouldBe true
-    Partial(ScoreVul(-230, Vulnerability.N)) shouldBe true
-    Partial(ScoreVul(-260, Vulnerability.N)) shouldBe true
-    Partial(ScoreVul(50, Vulnerability.N)) shouldBe false
-  }
-
-  it should "trickScore 1" in {
-    trickScorePredicate.justification(40) shouldBe Some("1 NT")
-    trickScorePredicate.justification(30) shouldBe Some("1 major")
-    trickScorePredicate.justification(20) shouldBe Some("1 minor")
-  }
-
-  it should "trickScore 2" in {
-    trickScorePredicate.justification(70) shouldBe Some("2 NT")
-    trickScorePredicate.justification(60) shouldBe Some("2 major")
-    trickScorePredicate.justification(40) shouldBe Some("1 NT")
-  }
-
-  it should "jLens" in {
-    val gameP: JPredicate[SB] = trickScorePredicate.jLens[SB](_ => "game")(stripBonus(500, 300))
-    gameP.justification(SB(620, vulnerability = true)) shouldBe Some("game 4 major")
-  }
-
   behavior of "PlayResult"
+  it should """apply("110")""" in {
+    val target = PlayResult("110")
+    target.r.isRight shouldBe true
+    target.r.toOption should matchPattern { case Some(110) => }
+    target.toString shouldBe "110"
+  }
+  it should """apply(Right(110))""" in {
+    val target = PlayResult(Right(110))
+    target.r.isRight shouldBe true
+    target.r.toOption should matchPattern { case Some(110) => }
+    target.toString shouldBe "110"
+  }
+  it should "apply(Left(DNP))" in {
+    val target = PlayResult(Left("DNP"))
+    target.r.isRight shouldBe false
+    target.r.toOption should matchPattern { case None => }
+    target.matchpoints(None) shouldBe None
+    target.toString shouldBe "DNP"
+  }
+  it should "apply(DNP)" in {
+    val target = PlayResult("DNP")
+    target.r.isRight shouldBe false
+    target.r.toOption should matchPattern { case None => }
+    target.matchpoints(None) shouldBe None
+    target.toString shouldBe "DNP"
+  }
+  it should "apply(A)" in {
+    val target = PlayResult("A")
+    target.r.isRight shouldBe false
+    target.matchpoints(None) shouldBe Some(Rational.half)
+    target.toString shouldBe "A"
+  }
+  it should "apply(A+)" in {
+    val target = PlayResult("A+")
+    target.r.isRight shouldBe false
+    target.matchpoints(None) shouldBe Some(Rational(3) / 5)
+    target.toString shouldBe "A+"
+  }
+  it should "apply(A-)" in {
+    val target = PlayResult("A-")
+    target.r.isRight shouldBe false
+    target.matchpoints(None) shouldBe Some(Rational(2) / 5)
+    target.toString shouldBe "A-"
+  }
+  it should "get probable contract 1" in {
+    val target = PlayResult(110)
+    target.getProbableContract(Vulnerability(1)) shouldBe Some("NS partial 2 major")
+  }
+  it should "output" in {
+    val score1 = "110"
+    val score2 = "100"
+    val result1 = PlayResult(score1)
+    val result2 = PlayResult(score2)
+    val bd = 5
+    val ns1 = 13
+    val ns2 = 14
+    val ew1 = 17
+    val ew2 = 16
+    val play1 = Play(ns1, ew1, result1)
+    val play2 = Play(ns2, ew2, result2)
+    val target = Traveler(bd, List(play1, play2), None)
+    target.board shouldBe bd
+    target.ps.size shouldBe 2
+    target.ps shouldBe List(play1, play2)
+    target.isPlayed shouldBe true
+    target.top shouldBe 1
+    val writer = MockWriter()
+    target.output(Output(writer)).close()
+    writer.spillway shouldBe
+      s"""Board: 5 with 2 plays
+         |NS pair	EW pair	NS score	NS MPs
+         |13	17	110	 1.00 (probable contract: NS partial 2 major)
+         |14	16	100	 0.00 (probable contract: EW  down 2)
+         |""".stripMargin
+  }
+
+  behavior of "checkScore"
 
   it should "checkScore partial" in {
     PlayResult(80).checkScore(B) shouldBe true
@@ -184,4 +132,5 @@ class PlayResultSpec extends AnyFlatSpec with should.Matchers {
     PlayResult(60).checkScore(B) shouldBe false
     PlayResult(-60).checkScore(B) shouldBe false
   }
+
 }
